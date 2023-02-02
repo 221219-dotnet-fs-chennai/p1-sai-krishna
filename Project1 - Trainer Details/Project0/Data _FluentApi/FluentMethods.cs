d88e3940bf6f2637cc3a036cd242f7e9f67ebb05
Sai Krishna <saikrishnahsr0@gmail.com>
2023-02-02 22:44:32 +0530

    Curd operations for skill table

--- Project1 - Trainer Details/Project0/Data _FluentApi/FluentMethods.cs
+++ Project1 - Trainer Details/Project0/Data _FluentApi/FluentMethods.cs
@@ -75,47 +75,7 @@ namespace Data__FluentApi
             return skills.ToList();
         }
 
-        //public void addSkill(Skill s)
-        //{
-        //    context.Skills.Add(s);
-        //    context.SaveChanges();
-        //    Console.WriteLine("skills added");
-        //}
-
-
-        //public List<Skill> GetSkil(int id)
-        //{
-
-
-        //    var skills = from s in context.Skills
-        //                 where s.TrainerId == id
-        //                 select s;
-        //    return skills.ToList();
-        //}
-
-
-
-        //public void removeSkill(Skill sk)
-        //{
-
-        //    var skillToBeRemoved = context.Skills.Where(s => s.SkillName == sk.SkillName).FirstOrDefault();
-        //    context.Skills.Remove(skillToBeRemoved);
-        //    context.SaveChanges();
-
-        //    Console.WriteLine("Skill Removed");
-
-        //}
-
-        ////public void updateSkill(Skill skill)
-        ////{
-
-        ////    var presentSills = GetSkil(id);
-        ////    var allSkills = from s in context.Skills
-        ////                    where s.TrainerId == s.TrainerId 
-        ////                    select s;
-
-
-        ////}
+       
 
 
     }
